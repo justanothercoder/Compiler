dcbf35386bbe98f15a516e1edb90807624c99faf
justanothercoder <yanushviktor@gmail.com>
2014-02-12 21:27:05 +0400

    fixed indent

--- variablesymboldefine.cpp
+++ variablesymboldefine.cpp
@@ -4,38 +4,38 @@ VariableSymbolDefine::VariableSymbolDefine(VariableSymbol *sym) : sym(sym) { }
 
 void VariableSymbolDefine::visit(GlobalScope *sc)
 {
-    sc->table[sym->getName()] = sym;
-    sc->addresses[sym] = sc->scope_size + GlobalConfig::int_size;
-    sc->scope_size += sym->getType()->getSize();
+	sc->table[sym->getName()] = sym;
+	sc->addresses[sym] = sc->scope_size + GlobalConfig::int_size;
+	sc->scope_size += sym->getType()->getSize();
 }
 
 void VariableSymbolDefine::visit(LocalScope *sc)
 {
-    sc->table[sym->getName()] = sym;
-    sc->addresses[sym] = sc->scope_size + GlobalConfig::int_size;
-    sc->scope_size += sym->getType()->getSize();
+	sc->table[sym->getName()] = sym;
+	sc->addresses[sym] = sc->scope_size + GlobalConfig::int_size;
+	sc->scope_size += sym->getType()->getSize();
 }
 
 void VariableSymbolDefine::visit(StructSymbol *sc)
 {
-    sc->table[sym->getName()] = sym;
-    sc->addresses[sym] = sc->scope_size;
-    sc->scope_size += sym->getType()->getSize();
-    sc->type_size += sym->getType()->getSize();
+	sc->table[sym->getName()] = sym;
+	sc->addresses[sym] = sc->scope_size;
+	sc->scope_size += sym->getType()->getSize();
+	sc->type_size += sym->getType()->getSize();
 }
 
 void VariableSymbolDefine::visit(FunctionSymbol *sc)
 {
-    sc->table[sym->getName()] = sym;
+	sc->table[sym->getName()] = sym;
 
-    if ( sym->isParam() )
-    {
-	sc->params_size += sym->getType()->getSize();
-	sc->addresses[sym] = -sc->params_size;
-    }
-    else
-    {
-	sc->addresses[sym] = sc->scope_size + GlobalConfig::int_size;
-	sc->scope_size += sym->getType()->getSize();
-    }
+	if ( sym->isParam() )
+	{
+		sc->params_size += sym->getType()->getSize();
+		sc->addresses[sym] = -sc->params_size;
+	}
+	else
+	{
+		sc->addresses[sym] = sc->scope_size + GlobalConfig::int_size;
+		sc->scope_size += sym->getType()->getSize();
+	}
 }
