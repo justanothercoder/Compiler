84a316dd2de35324298a417d894dd739314d8264
justanothercoder <yanushviktor@gmail.com>
2014-03-23 09:00:50 +0400

    remove unneeded code

--- dotnode.cpp
+++ dotnode.cpp
@@ -55,11 +55,7 @@ void DotNode::gen(const TemplateInfo& template_info)
 		}
 		else
 			member = new VariableSymbol(ov_func->getName(), VariableType(std::begin(ov_func_type_info.symbols)->second));
-
-//		CodeGen::emit("lea rax, [" + static_cast<FunctionSymbol*>(member->getType().type)->getScopedTypedName() + "]");
 	}
-//	else if ( member_type.type->getTypeKind() == TypeKind::FUNCTION ) 
-//		CodeGen::emit("lea rax, [" + static_cast<FunctionSymbol*>(member->getType().type)->getScopedTypedName() + "]");
 	else
 		CodeGen::emit("lea rax, [rax - " + std::to_string(base_type->get_valloc()->getAddress(member)) + "]");
 }
