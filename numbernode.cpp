dda39050ef0cf05bc7dc919f9e1839235a190ca9
justanothercoder <yanushviktor@gmail.com>
2014-03-23 10:14:10 +0400

    fixed bug in numbernode

--- numbernode.cpp
+++ numbernode.cpp
@@ -4,14 +4,14 @@ NumberNode::NumberNode(string num) : num(num) { }
 
 VariableType NumberNode::getType() const { return VariableType(BuiltIns::int_struct, false, true); }
 
-void NumberNode::check(const TemplateInfo&) { }
+void NumberNode::check(const TemplateInfo&) { getScope()->get_valloc()->addLocal(getType().getSize()); }
 
 AST* NumberNode::copyTree() const { return new NumberNode(num); }
 
 void NumberNode::gen(const TemplateInfo&)
 {
-    CodeGen::emit("mov qword [rsp - " + std::to_string(getScope()->get_valloc()->getAddressForLocal() + getType().getSize()) + "], " + num);
-	CodeGen::emit("lea rax, [rsp - " + std::to_string(getScope()->get_valloc()->getAddressForLocal() + getType().getSize()) + "]");
+    CodeGen::emit("mov qword [rbp - " + std::to_string(getScope()->get_valloc()->getAddressForLocal() + getType().getSize()) + "], " + num);
+	CodeGen::emit("lea rax, [rbp - " + std::to_string(getScope()->get_valloc()->getAddressForLocal() + getType().getSize()) + "]");
 }
 	
 vector<AST*> NumberNode::getChildren() const { return { }; }
